6ce3138be121a63f84c4a33bf6eba0d8bd214f4a
arfemia <andyfemia@gmail.com>
2024-04-02 22:18:17 -0400

    Update TileCard.tsx

--- src/components/TileCard.tsx
+++ src/components/TileCard.tsx
@@ -7,9 +7,11 @@ export function TileCard({ tile }: { tile: TileModel }) {
   return (
     <div className="bg-slate-900 my-2  border-slate-700 border-[1px] rounded-xl shadow-md flex flex-col w-full max-w-2xl">
       <div className="flex flex-row justify-between">
-        <h2 className="text-2xl font-bold px-4 py-2 ">{tile.title}</h2>
+        <h2 className="text-xl sm:text-2xl font-bold px-4 py-2 ">
+          {tile.title}
+        </h2>
         <button
-          className="bg-slate-900 rounded-xl px-2 hover:bg-slate-700 text-slate-500 font-mono text-xs"
+          className="bg-slate-900 rounded-xl px-2 hover:bg-slate-700 text-slate-500 font-mono text-sm"
           onClick={() => {
             toast.info("Coming Soon");
           }}
@@ -18,25 +20,26 @@ export function TileCard({ tile }: { tile: TileModel }) {
         </button>
       </div>
       <Divider />
+      <p className=" px-4 py-1 text-slate-400 font-mono">
+        {"Due At: " + tile.endDate.toDate().toLocaleString()}
+      </p>
+
+      <div className="px-4 ">
+        <ShiftingCountdown date={tile.endDate.toDate()}></ShiftingCountdown>
+      </div>
+
+      <Divider className="mt-2" />
+
       {tile.description?.length !== 0 && (
         <p className=" px-4 text-slate-400 py-1 text-sm">{tile.description}</p>
       )}
 
-      <b className=" px-4 py-1 text-slate-400">
-        {"Due At: " + tile.endDate.toDate().toLocaleString()}
-      </b>
-
-      <Divider />
-
-      <div className="px-4 py-2">
-        <ShiftingCountdown date={tile.endDate.toDate()}></ShiftingCountdown>
-      </div>
-      <i className="text-slate-500 px-4 font-mono text-sm">
-        {"Updated At: " + tile.updatedAt.toDate().toLocaleString()}
+      <i className="text-slate-500 px-4 font-mono text-xs  md:text-sm py-1 sm:py-2">
+        {"Last Updated: " + tile.updatedAt.toDate().toLocaleString()}
       </i>
-      <i className="text-slate-500 px-4 font-mono pb-1 text-sm">
+      {/* <i className="text-slate-500 px-4 font-mono pb-1 text-sm">
         {"Created At: " + tile.createdAt.toDate().toLocaleString()}
-      </i>
+      </i> */}
     </div>
   );
 }
